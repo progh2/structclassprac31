a881a10429637c9a6047d629c9498b3690dfd388
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-04 14:43:11 +0900

    클래스와 구조체의 복사 비교

--- structclassprac31/Program.cs
+++ structclassprac31/Program.cs
@@ -8,8 +8,40 @@ namespace structclassprac31
 {
     internal class Program
     {
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x; this.y = y;
+            }
+        }
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+            public PointStruct(int x, int y)
+            {
+                this.x = x; this.y = y;
+            }
+        }
         static void Main(string[] args)
         {
+            PointClass pcA = new PointClass(10, 20);
+            PointClass pcB = pcA;
+            pcB.x = 100;
+            pcB.y = 200;
+            Console.WriteLine($"pcA: {pcA.x}, {pcA.y}");
+            Console.WriteLine($"pcB: {pcB.x}, {pcB.y}");
+            Console.WriteLine();
+            PointStruct psA = new PointStruct(10, 20);
+            PointStruct psB = psA;
+            psB.x = 100;
+            psB.y = 200;
+            Console.WriteLine($"psA: {psA.x}, {psA.y}");
+            Console.WriteLine($"psB: {psB.x}, {psB.y}");
+            Console.WriteLine();
         }
     }
 }
